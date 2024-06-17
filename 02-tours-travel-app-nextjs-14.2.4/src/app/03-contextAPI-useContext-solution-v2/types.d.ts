a916388485b1b74c2212371278f960852a561483
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-06-17 19:41:25 +0200

    project 2: tours travel: 03 contextAPI & useContext  solution V2

---
+++ 02-tours-travel-app-nextjs-14.2.4/src/app/03-contextAPI-useContext-solution-v2/types.d.ts
@@ -0,0 +1,38 @@
+interface ITour {
+  id: string;
+  name: string;
+  info: string;
+  image: string;
+  price: number;
+}
+
+interface IFetchToursState {
+  isLoading: boolean;
+  isError: boolean;
+  errorMessage: string;
+  tours: ITour[] | [];
+}
+
+interface ITourContext {
+  isLoading: boolean;
+  isError: boolean;
+  errorMessage: string;
+  tours: ITour[];
+  removeTourItem: (id: string) => void;
+  fetchTours: () => Promise<void | undefined>;
+}
+
+interface ITourContextState {
+  isLoading: boolean;
+  isError: boolean;
+  errorMessage: string;
+  tours: ITour[];
+}
+
+interface ITourContextProviderProps {
+  children: React.ReactNode;
+}
+
+interface ITourItemProps {
+  tour: ITour;
+}
