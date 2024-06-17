f7ed4f8ca3549ef4ba2732b1442235aecc6a35f0
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-06-17 19:44:00 +0200

    project 2: tours travel: 06 useReducer & useContext solution

---
+++ 02-tours-travel-app-nextjs-14.2.4/src/app/06-useReducer-useContext-solution/type.d.ts
@@ -0,0 +1,27 @@
+interface ITour {
+  id: string;
+  name: string;
+  info: string;
+  image: string;
+  price: string;
+}
+
+interface IFetchToursState {
+  isLoading: boolean;
+  isError: boolean;
+  errorMessage: string;
+  tours: ITour[] | [];
+}
+
+type TypeToursAction =
+  | { type: "REMOVE_ONE_TOUR"; payload: string }
+  | { type: "RESET_ALL_TOURS" }
+  | { type: "START_FETCH_TOURS" }
+  | { type: "HANDLE_FETCH_ERROR"; payload: string }
+  | { type: "HANDLE_FETCH_SUCCESS"; payload: ITour[] }
+  | { type: "HANDLE_ERROR" };
+
+type TypeErrorMessage = {
+  code: number;
+  message: string;
+};
